b2473b5dee7d58e81dd8d7916cfcb7062a3724e2
Darryl Staflund <d.staflund@gmail.com>
2023-11-21 14:03:39 -0700

    chore: Initial check-in

--- jenkinsfile-pipeline.groovy
+++ jenkinsfile-pipeline.groovy
@@ -13,6 +13,23 @@ node('Jenkins-Agent') {
         ])
     }
 
-    def flow = load pwd() + '\\jenkinsfile.groovy'
-    flow.buildAndRun()
+    def resolvedAntHome = tool name: 'apache-ant-1.10.14', type: 'hudson.tasks.Ant$AntInstallation'
+    def resolvedJdkHome = tool name: 'jdk-17.0.2', type: 'hudson.model.JDK'
+
+    withEnv([
+            "ANT_HOME=${resolvedAntHome}"
+            ,"JAVA_HOME=${resolvedJdkHome}"
+            ,"PATH=${resolvedAntHome}/bin;${resolvedJdkHome}/bin;${env.PATH}"
+    ]) {
+        stage('Build') {
+            dir("./ant-sample") {
+                bat "${env.ANT_HOME}\\bin\\ant.bat -f ${env.WORKSPACE}\\build.xml"
+            }
+        }
+        stage('Run') {
+            dir("./ant-sample") {
+                bat "${env.JAVA_HOME}\\bin\\java.exe -jar ${env.WORKSPACE}\\build\\jar\\ant-sample.jar"
+            }
+        }
+    }
 }
